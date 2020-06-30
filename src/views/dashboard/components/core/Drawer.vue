10c07500c8f056ae9169bd7b6f5c9a82f149b732
wesbubeck <wesb@pendo.io>
2020-06-30 01:20:16 -0400

    set up env

--- src/views/dashboard/components/core/Drawer.vue
+++ src/views/dashboard/components/core/Drawer.vue
@@ -164,14 +164,14 @@
         }
       },
       teamEnvUrlAndSubLink () {
-        const baseUrl = '' // add you baseUrl here ex. 'https://app.pendo-perfserf.pendo-dev.com/'
-        const subId = '' // add your subId here ex. '5634161670881280'
+        const baseUrl = 'https://app.pendo-perfserf.pendo-dev.com/'
+        const subId = '5704134103662592'
         const base64EncodedSearchParams = window.btoa(`?after=&subId=${subId}`)
         return `${baseUrl}/deep-links/${base64EncodedSearchParams}`
       },
       teamEnvUrlAndSub () {
-        const teamEnv = '' // add the name of your team env ex. 'perf-serfs'
-        const subName = '' // add your subName ex. 'pendo-internal'
+        const teamEnv = 'perf-serfs'
+        const subName = 'Apollo'
         return `${teamEnv} ${subName}`
       },
     },
--- vue.config.js
+++ vue.config.js
@@ -14,6 +14,6 @@ module.exports = {
     },
   },
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/your-repo-name/'
+    ? '/perf-serf-apollo/'
     : '/',
 }
